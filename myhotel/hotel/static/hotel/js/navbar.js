f18bc88ba5a4d7a3e8ddd889ebab75703fd45b84
mandel <nelsonokiki@gmail.com>
2025-07-01 09:47:32 +0100

    update the navbar js file

---
+++ myhotel/hotel/static/hotel/js/navbar.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let scrollPos = 0;
+    const mainNav = document.getElementById('mainNav');
+    const headerHeight = mainNav ? mainNav.clientHeight : 0;
+
+    if (mainNav) {
+        window.addEventListener('scroll', function () {
+            const currentTop = document.body.getBoundingClientRect().top * -1;
+            if (currentTop < scrollPos) {
+                if (currentTop > 0 && mainNav.classList.contains('is-fixed')) {
+                    mainNav.classList.add('is-visible');
+                } else {
+                    mainNav.classList.remove('is-visible', 'is-fixed');
+                }
+            } else {
+                mainNav.classList.remove('is-visible');
+                if (currentTop > headerHeight && !mainNav.classList.contains('is-fixed')) {
+                    mainNav.classList.add('is-fixed');
+                }
+            }
+            scrollPos = currentTop;
+        });
+    }
+});
