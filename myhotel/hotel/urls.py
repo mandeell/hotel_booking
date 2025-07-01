1093360e24e251ef67d13b9560a9215ccceabc28
mandel <nelsonokiki@gmail.com>
2025-07-01 09:55:30 +0100

    add the payment url

--- myhotel/hotel/urls.py
+++ myhotel/hotel/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from . import views
+from . import views, booking
+from .booking import submit_booking
 from .views import check_availability_ajax, home, contact_submit
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('check-availability', check_availability_ajax, name='check_availability_ajax'),
     path('contact-submit', contact_submit, name='contact_submit'),
+    path('submit-booking', submit_booking, name='submit_booking'),
+    path('verify-payment', booking.verify_payment, name='verify_payment'),
+    path('store-expected-amount', booking.store_expected_amount, name='store_expected_amount'),
+    path('webhook', booking.webhook, name='webhook'),
 ]
