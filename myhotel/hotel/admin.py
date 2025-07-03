4ef277c32ef3966a9371ab7fcacee444ee9f0ba4
mandel <nelsonokiki@gmail.com>
2025-07-03 10:07:27 +0100

    register the models to the admin.py

--- myhotel/hotel/admin.py
+++ myhotel/hotel/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Booking, Room, Hotel, RoomType, Guest, RoomAmenity, HotelAmenity
 
-# Register your models here.
+# Register your models here with the default custom_admin site.
 admin.site.register(Hotel)
 admin.site.register(RoomType)
 admin.site.register(Room)
@@ -9,5 +9,3 @@ admin.site.register(Booking)
 admin.site.register(Guest)
 admin.site.register(RoomAmenity)
 admin.site.register(HotelAmenity)
-
-
